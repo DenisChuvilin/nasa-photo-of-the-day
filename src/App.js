dc1b506b0bfe3a486cba70ef06d549422d989f1c
DenisChuvilin <denischuv@gmail.com>
2020-03-18 15:55:13 -0700

    photo appears

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import "./App.css";
+import Background from "./components/Background";
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-
+<Background/>
     </div>
   );
 }
