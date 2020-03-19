6193704d34f3d4937654e3e5f82d8e73bc42f7b3
DenisChuvilin <denischuv@gmail.com>
2020-03-19 14:14:41 -0700

    second turn in

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import ToggleDesc from "./ToggleDesc";
+import {FaChevronLeft,FaChevronRight} from "react-icons/fa"; 
 //styled components
 const Date = styled.h5 `
 margin-bottom: 20%;
@@ -18,10 +19,22 @@ color: rgb(211,211,211,.3);
 ${'' /* border: 1px dotted red; */}
 font-family: 'Montserrat', sans-serif;
 `
+// arrow click handlers
+function leftClickHandler() {
+
+};
+
+function rightClickHandler() {
+
+};
 
 const Home = ({apod}) => (
     <>
-   <Date>{apod.date}</Date>
+   <Date>
+   <button><FaChevronLeft onClick = {leftClickHandler} /></button>
+   {apod.date}
+   <button><FaChevronRight onClick = {rightClickHandler}/></button>
+   </Date>
     <ApodTitle>{apod.title}</ApodTitle>
    <ToggleDesc desc = {apod.explanation}/>
    </>
